8dc04e424b452cc1b31e991d1482091c9dd2ab2e
Mohammed Sharook <msharuk589@gmail.com>
2025-08-19 21:35:23 +0530

    Modified the colab file and streamlit config file to take the latest best model automatically without hardcoding it

--- src/config.py
+++ src/config.py
@@ -1,7 +1,15 @@
+import os 
+import glob
 import warnings
 import pandas as pd
 
 pd.set_option('display.max_columns', None)
 warnings.filterwarnings('ignore')
 
-MODEL_FILE_NAME = 'insurance_cost_prediction_best_model_rf.pkl'
+model_files = glob.glob("insurance_cost_predictions_best_model_*.pkl")
+if model_files:
+    MODEL_FILE_NAME = max(model_files, key=lambda x: os.path.getmtime(x))
+else:
+    MODEL_FILE_NAME = None
+
+# MODEL_FILE_NAME = 'insurance_cost_prediction_best_model_rf.pkl'
