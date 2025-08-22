249f60a81fe8978eef01e5b8e4d6c4fc137cb5f4
Mohammed Sharook <msharuk589@gmail.com>
2025-08-22 10:43:21 +0530

    resolved age related issues in app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ col1, col2 = st.columns(2)
 
 with col1:
     st.header("Personal Information")
-    age = st.slider("Age", min_value=18, max_value=100, value=24, help="Your current age")
+    age = st.slider("Age", min_value=18, max_value=66, value=24, help="Your current age")
     height = st.number_input("Height (cm)", min_value=100, max_value=250, value=163, help="Your height in centimeters")
     weight = st.number_input("Weight (kg)", min_value=30, max_value=200, value=75, help="Your weight in kilograms")
     surgeries = st.selectbox("Number of Major Surgeries", options=[0, 1, 2, 3, 4, 5], help="Number of major surgeries you've had")
